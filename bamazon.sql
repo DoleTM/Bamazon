de8aba770281b7c20eb15c430dddfb065b21a9f6
DoleTM <adylantoremember@gmail.com>
2019-02-15 00:35:39 -0500

    Commit #2

--- bamazon.sql
+++ bamazon.sql
@@ -21,15 +21,16 @@ CREATE TABLE products (
 );
 
 -- Adds data to the products table in the bamazon database --
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Bells Hopslam", "IPA", 10.45, 60);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("North Coast Old Rasputin", "Stout", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Cigar City Jai Alai", "IPA", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Playalinda Liquid Stupid", "Belgian Strong", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Coppertail Unholy", "Belgian Trippel", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Orange Blossom Pilsner 2", "Pilsner", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Sixpoint Resin", "DIPA", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Brew Bus Hazelnut Spread", "Porter", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Dogfish Head 120 Minute", "IPA", 10, 40);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("Cigar City Togobonga", "Ale", 10, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values 
+("Bells Hopslam", "IPA", 10.45, 60),
+("North Coast Old Rasputin", "Stout", 10, 40),
+("Cigar City Jai Alai", "IPA", 10, 40),
+("Playalinda Liquid Stupid", "Belgian Strong", 10, 40),
+("Coppertail Unholy", "Belgian Trippel", 10, 40),
+("Orange Blossom Pilsner 2", "Pilsner", 10, 40),
+("Sixpoint Resin", "DIPA", 10, 40),
+("Brew Bus Hazelnut Spread", "Porter", 10, 40),
+("Dogfish Head 120 Minute", "IPA", 10, 40),
+("Cigar City Togabonga", "Ale", 10, 40);
 
-SELECT * FROM products;
+SELECT * FROM bamazon_db.products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,89 @@
+// Declare global variables
 var inquirer = require("inquirer");
+var mysql = require("mysql");
 
-var bamazon = require("bamazon.sql");
-
-inquirer.prompt([
-    {
-        type: "input",
-        name: "bamazon",
-        message: "Please select the the item_id number you would you like to buy"
-    },
-    {
-        type: "input",
-        name: "quantity",
-        message: "Which quantity would you like to purchase?"
-    }
-]).then(function (response){
-    console.log(response);
+// Establish Connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "yourRootPassword",
+    database: "bamazon_db"
 });
+
+// While in the connection do something....
+connection.connect(function (err) {
+    if (err) throw err;
+    // Thread id #
+    console.log("Connected as ID: " + connection.threadId);
+
+    readTable();
+
+    initiate();
+
+});
+
+function initiate() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "Please select the the item_id number you would you like to buy\n"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "Which quantity would you like to purchase?"
+        }
+    ]).then(function (response) {
+        console.log(response);
+    });
+    connection.end();
+};
+
+// CRUD functions
+function readTable() {
+    // Selects all info from the database and loops through the table
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("=========================================");
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        };
+        console.log("=========================================");
+    });
+};
+
+// function updateTable() {
+//     console.log("Updating all Rocky Road quantities...\n");
+//      var reduce = (stock_quantity - repsonse.quantity)
+//     var query = connection.query(
+//         "UPDATE products SET ? WHERE ?",
+//         [
+//             {
+//                 stock_quantity: response.
+//             }
+//         ],
+//         function (err, res) {
+//             console.log(res.affectedRows + " products updated!\n");
+//             // Call deleteProduct AFTER the UPDATE completes
+//             deleteProduct();
+//         }
+//     );
+
+//     // logs the actual query being run
+//     console.log(query.sql);
+// };
+
+
+// inquirer.prompt([{}]).then(function (res){
+//     connection.query(
+//         "INSERT INTO",
+//         {
+//             item_name: res.item
+//         },
+//         function (err) {
+//             if (err) throw err;
+//         }
+//     )
+// })
